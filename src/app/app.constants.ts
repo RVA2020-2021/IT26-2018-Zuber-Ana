7b2fb9ddba53ef0e379233081945c7e33660afd0
AnaZuber <ana.zuber99@gmail.com>
2021-06-09 11:40:48 +0200

    endpoint

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,7 +1,13 @@
-export const TIPRACUNA_URL = 'http://localhost:8084/tipRacuna';
+/*export const TIPRACUNA_URL = 'http://localhost:8084/tipRacuna';
 export const KLIJENT_URL = 'http://localhost:8084/klijent';
 export const KREDIT_URL = 'http://localhost:8084/kredit';
 export const RACUN_URL = 'http://localhost:8084/racun';
 export const RACUN_PO_KLIJENTU_URL = 'http://localhost:8084/racunPoKlijentu';
-export const RACUN_PO_TIPU_URL = 'http://localhost:8084/racunPoTipuRacuna';
+export const RACUN_PO_TIPU_URL = 'http://localhost:8084/racunPoTipuRacuna';*/
 
+export const TIPRACUNA_URL = 'https://rva-backend-it-26-2018.herokuapp.com/tipRacuna';
+export const KLIJENT_URL = 'https://rva-backend-it-26-2018.herokuapp.com/klijent';
+export const KREDIT_URL = 'https://rva-backend-it-26-2018.herokuapp.com/kredit';
+export const RACUN_URL = 'https://rva-backend-it-26-2018.herokuapp.com/racun';
+export const RACUN_PO_KLIJENTU_URL = 'https://rva-backend-it-26-2018.herokuapp.com/racunPoKlijentu';
+export const RACUN_PO_TIPU_URL = 'https://rva-backend-it-26-2018.herokuapp.com/racunPoTipuRacuna';
